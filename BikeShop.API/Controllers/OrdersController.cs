976f10ef480f4706bbd8e53e7fb8a25151d311dc
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-08-21 23:36:40 +0300

    Implement CreateOrder API

---
+++ BikeShop.API/Controllers/OrdersController.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Claims;
+using BikeShop.API.Mappers;
+using BikeShop.Application.Abstractions.Messaging;
+using BikeShop.Application.Carts.AddItem;
+using BikeShop.Application.Common.Models;
+using BikeShop.Application.Orders.CreateOrder;
+using BikeShop.Application.Orders.DTOs;
+using BikeShop.Application.Orders.Requests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeShop.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ICommandHandler<CreateOrderCommand, Result<CreateOrderResultDto>> _createOrderCommandHandler;
+
+        public OrdersController(ICommandHandler<CreateOrderCommand, Result<CreateOrderResultDto>> createOrderCommandHandler)
+        {
+            _createOrderCommandHandler = createOrderCommandHandler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderRequest request,  CancellationToken cancellationToken)
+        {
+            var userIdResult = GetApplicationUserId();
+            if (userIdResult.IsFailure)
+                return this.ToActionResult(userIdResult.Error!);
+
+            var applicationUserId = userIdResult.Data;
+            var result = await _createOrderCommandHandler.Handle(
+                new CreateOrderCommand(applicationUserId, request.DeliveryAddress, request.DeliveryAt, request.CustomerPhone), 
+                cancellationToken);
+
+            if (result.IsFailure) {
+                return this.ToActionResult(result.Error!);
+            }
+
+            return Ok(result.Data);
+        }
+
+        private Result<int> GetApplicationUserId()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (!int.TryParse(userId, out var applicationUserId))
+                return Result<int>.Failure(new Error(ErrorCode.Validation, "Invalid user identity"));
+
+            return Result<int>.Success(applicationUserId);
+        }
+    }
+}
---
+++ BikeShop.Application/Orders/Requests/CreateOrderRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace BikeShop.Application.Orders.Requests
+{
+    public sealed record CreateOrderRequest (string DeliveryAddress, DateTime DeliveryAt, string CustomerPhone);
+}
